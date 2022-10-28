fafcc7934f037faa458ce85be6e44bb9f10b0f25
eugeniozizeron <eugenio@zizeron.tech>
2022-10-28 13:28:04 +0200

    config mock service worker

--- src/pages/summary/test/SummaryForm.test.js
+++ src/pages/summary/test/SummaryForm.test.js
@@ -1,4 +1,5 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import SummaryForm from "../SummaryForm";
 
@@ -16,7 +17,8 @@ test("initial state", () => {
   expect(button).toBeDisabled();
 });
 
-test("enable button in first click and disable button in second click", () => {
+test("enable button in first click and disable button in second click", async () => {
+  const user = userEvent.setup();
   render(<SummaryForm />);
   const checkbox = screen.getByRole("checkbox", {
     name: /terms and conditions/i,
@@ -24,10 +26,31 @@ test("enable button in first click and disable button in second click", () => {
   const button = screen.getByRole("button", { name: /confirm order/i });
 
   // click on checkbox to enable button
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(button).toBeEnabled();
 
   // click again on checkbox to disable button
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(button).toBeDisabled();
+});
+
+test("popover responds to hover", async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm />);
+
+  // popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // popover appears on mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  // popover disappear when we mouse out
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
 });
