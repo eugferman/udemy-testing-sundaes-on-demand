003da30ab8b919c65daafb5a54d111ba36e7ebf0
eugeniozizeron <eugenio@zizeron.tech>
2022-11-02 12:10:05 +0100

    code and test grand total

--- src/pages/entry/OrderEntry.js
+++ src/pages/entry/OrderEntry.js
@@ -1,10 +1,15 @@
 import Options from "./Options";
+import { useOrderDetails } from "../../context/orderDetails";
+import { formatCurrency } from "../../utilities";
 
 const OrderEntry = () => {
+  const { totals } = useOrderDetails();
   return (
     <div>
+      <h1>Design Your Sundae!</h1>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
+      <h2>Grand total: {formatCurrency(totals.scoops + totals.toppings)}</h2>
     </div>
   );
 };
